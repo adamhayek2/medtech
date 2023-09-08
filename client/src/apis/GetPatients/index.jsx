510745256d7e3a26b52abde2affad2bb4ae932a1
adamhayek2002 <YOUR_EMAIL>
2023-09-08 16:27:11 +0300

    Front-End <> Creating API to get patients

---
+++ client/src/apis/GetPatients/index.jsx
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const apiUrl = 'http://127.0.0.1:8000/api/user/all_patients';
+
+const GetPatients = async () => {
+    try {
+        const response = await axios.get(apiUrl, {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`
+            }
+          });
+      return response.data.data;
+    } catch (error) {
+      console.error('Error fetching posts', error);
+    }
+  };
+export default GetPatients
