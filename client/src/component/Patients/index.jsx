c42b01bf4876a1d2727bc193d37a2d287136b381
adamhayek2002 <YOUR_EMAIL>
2023-09-14 08:41:38 +0300

    Front-End <> Adding search input to the patients page

--- client/src/component/Patients/index.jsx
+++ client/src/component/Patients/index.jsx
@@ -2,10 +2,12 @@ import React, {useState, useEffect} from 'react'
 import PatienCard from '../PatientCard'
 import PageTitle from '../PageTite'
 import GetPatients from '../../apis/GetPatients'
+import SearchInput from '../base/SearchInput'
 
 const Patients = () => {
     const [patients, setPatient] = useState([]);
     const [error, setError] = useState(false);
+
 
     const fetchPatients = async () => {
     try {
@@ -26,6 +28,7 @@ const Patients = () => {
   return (
     <div className='min-h-screen w-5/6 flex flex-col gap-14 bg-grey p-14 ml-auto'>
         <PageTitle title={"Patients"}/>
+        <SearchInput />
         <div className='flex flex-row flex-wrap gap-10 justify-between'>
             {patients.map((patient) => (
             <PatienCard key={patient.id} id={patient.id} name={patient.full_name} status={patient.status.name}/>
