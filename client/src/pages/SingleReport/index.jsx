35563d7c2cab0f03fbd4bae980285c83174b4cfc
adamhayek2002 <YOUR_EMAIL>
2023-09-15 11:07:00 +0300

    Front-End <> Adding Navigations Bar to the Sigle report page

--- client/src/pages/SingleReport/index.jsx
+++ client/src/pages/SingleReport/index.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import NavBar from '../../component/NavBar'
+import SideNav from '../../component/SideNav'
 
 const SingleReport = () => {
   return (
-    <div>SingleReport</div>
+    <div className='flex flex-col font-futur'>
+      <NavBar/> 
+      <div className='flex flex-row'>
+        <SideNav/>
+        
+      </div>
+    </div>
   )
 }
 
