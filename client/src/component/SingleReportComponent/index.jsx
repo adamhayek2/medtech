7a370a4e1e093d5a170e0226cac20fffc5c14737
adamhayek2002 <YOUR_EMAIL>
2023-09-16 16:40:47 +0300

    Front-End <> Adding the tests block to single report

--- client/src/component/SingleReportComponent/index.jsx
+++ client/src/component/SingleReportComponent/index.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import { useParams } from 'react-router-dom';
+import { ReactComponent as BloodTestSVG } from "../../resources/svg/blood-test.svg";
 import SingleReport from '../../apis/SingleReport';
 import PageTitle from '../PageTite';
 import Button from '../base/Button';
@@ -43,7 +44,7 @@ const SingleReportComponent = () => {
                         <h1 className='text-[#7D7D7D]'>Status:</h1>
                         <p className=' text-[22px] text-primary font-bold'> {report.patient_status}</p>
                     </div>
-                    <div className='flex flex-col px-16 py-8 bg-white rounded-lg gap-[22px] items-center '>
+                    <div className='flex flex-col px-14 py-8 bg-white rounded-lg gap-[22px] items-center '>
                         <h1 className='text-[22px] font-bold text-primary'>Informations</h1>
                         <div className='flex flex-col items-center gap-1'>
                             <p className='text-[#7D7D7D]'>Gender: {report.gender}</p>
@@ -54,6 +55,23 @@ const SingleReportComponent = () => {
                     </div>
                 </div>
                 <div className='w-4/5'>
+                <div className='flex flex-col px-14 py-8 bg-white rounded-lg gap-10 items-start '>
+                        <h1 className='text-[22px] font-bold text-primary'>Tests</h1>
+                        <div className='flex flex-row items-center gap-10'>
+                        {report.report_data.blood_tests.map((tem) => (
+                            <div className='flex flex-row items-center gap-3'>
+                                <BloodTestSVG/>
+                                <div className='flex flex-col '>
+                                    <div className='text-[18px]'>{tem.test_name}</div>
+                                    <div className='text-base text-[#7D7D7D] italic'>{tem.date}</div>
+                                </div>
+                            </div>
+                        ))}
+                        </div>
+                        <div className='w-full flex flex-row justify-end'>
+                            <Button label={report.approved_by_doctor_id === 0 ? 'See all' : 'Edit'}/>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
