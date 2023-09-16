61bb2b2d615387cb561005b268e9be26873476d7
adamhayek2002 <YOUR_EMAIL>
2023-09-16 12:42:58 +0300

    Front-End <> Editing styles and Adding the first block if patient information

--- client/src/component/SingleReportComponent/index.jsx
+++ client/src/component/SingleReportComponent/index.jsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react'
 import { useParams } from 'react-router-dom';
 import SingleReport from '../../apis/SingleReport';
 import PageTitle from '../PageTite';
+import Button from '../base/Button';
 
 const SingleReportComponent = () => {
     const { id } = useParams();
@@ -24,15 +25,21 @@ const SingleReportComponent = () => {
       }, []);
 
   return (
-    <div className='min-h-screen w-5/6 flex flex-col gap-14 bg-grey p-14 ml-auto'>
+    <div className='min-h-screen w-5/6 ml-auto'>
     {report.length === 0 || error ? 
         <div>no reports</div> : 
-        <div>
+        <div className='min-h-screen flex flex-col gap-14 bg-grey p-14'>
             <div className='w-full flex flex-row justify-between'>
             <PageTitle title={`Report ${report.id}`}/>
             </div>
-            <div className='flex flex-row flex-wrap gap-10 justify-between'>
-            
+            <div className='w-1/5 flex flex-row flex-wrap gap-10 justify-between'>
+                <div className='flex flex-col h-full w-full'>
+                    <div className='flex flex-col px-16 py-12 bg-white rounded-lg gap-[22px] items-center'>
+                        <h1 className='text-[22px] font-bold text-primary'>{report.patient_name}</h1>
+                        <p className='text-[#7D7D7D]'>Patient ID: {report.patient.id}</p>
+                        <Button label={'View Profile'}/>
+                    </div>
+                </div>
             </div>
         </div>
         }
