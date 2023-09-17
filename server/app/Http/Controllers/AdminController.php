7df5835d31b37c67157c9f844b9777b1250a7337
adamhayek2002 <YOUR_EMAIL>
2023-09-17 20:41:32 +0300

    Back-End <> Adding more statisticas to the dashboard API

--- server/app/Http/Controllers/AdminController.php
+++ server/app/Http/Controllers/AdminController.php
@@ -4,21 +4,37 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\AiReport;
+use App\Models\Patient;
 
 class AdminController extends Controller
 {
     public function dashboard() {
-        $reportsTodayCount = AiReport::whereDate('created_at', now()->toDateString())->count();
-
+        $todaysReports = AiReport::whereDate('created_at', now()->toDateString());
+        $reportsTodayCount = $todaysReports->count();
+        
         $mostRepeatedLabel = AiReport::select('label')
             ->groupBy('label')
             ->orderByRaw('COUNT(*) DESC')
             ->limit(1)
             ->value('label');
 
+        $todaysPatients = Patient::whereDate('updated_at', now()->toDateString())->get();
+        $timeDifferences = [];
+
+        foreach ($todaysPatients as $patient) {
+            $report = AiReport::find($patient->id);
+
+            // $timeDifferences[$patient->id] = $report;
+            if ($report) {
+                $timeDifference = $patient->updated_at->diffInHours($report->created_at);
+                $timeDifferences[$patient->id] = $timeDifference;
+            }
+        }
+
         return response()->json([
             'reportsTodayCount' => $reportsTodayCount,
             'mostRepeatedLabel' => $mostRepeatedLabel,
+            'timeDifferences' => $timeDifferences,
         ]);
     }
 }
