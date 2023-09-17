2351997cfb0f2383e63313b3c95ee0637f932147
adamhayek2002 <YOUR_EMAIL>
2023-09-17 20:44:39 +0300

    Front-End <> Adding navbar and sidnave to the dashboard page

--- client/src/pages/Dashboard/index.jsx
+++ client/src/pages/Dashboard/index.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import NavBar from '../../component/NavBar'
+import SideNav from '../../component/SideNav'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className='flex flex-col font-futur'>
+      <NavBar/> 
+      <div className='flex flex-row'>
+        <SideNav/>
+        
+      </div>
+    </div>
   )
 }
 
