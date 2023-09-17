02207243ba468a21f6fba8b65023850fc3dc6181
adamhayek2002 <YOUR_EMAIL>
2023-09-17 12:13:25 +0300

    Back-End <> Creating the dashboard function to retrieve statistics

--- server/app/Http/Controllers/AdminController.php
+++ server/app/Http/Controllers/AdminController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\AiReport;
 
 class AdminController extends Controller
 {
-    //
+    public function dashboard() {
+        $reportsTodayCount = AiReport::whereDate('created_at', now()->toDateString())->count();
+
+        $mostRepeatedLabel = AiReport::select('label')
+            ->groupBy('label')
+            ->orderByRaw('COUNT(*) DESC')
+            ->limit(1)
+            ->value('label');
+
+        return response()->json([
+            'reportsTodayCount' => $reportsTodayCount,
+            'mostRepeatedLabel' => $mostRepeatedLabel,
+        ]);
+    }
 }
