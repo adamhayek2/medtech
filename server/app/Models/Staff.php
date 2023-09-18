32e8d592122ac456a917b38c8d0f53a56d0852a2
adamhayek2002 <YOUR_EMAIL>
2023-09-18 10:29:09 +0300

    Back-End <> Adding Staff model and defining its relations

---
+++ server/app/Models/Staff.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Staff extends Model
+{
+    use HasFactory;
+
+    public function user() {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function department() {
+        return $this->belongsTo(Department::class, 'department_id');
+    }
+
+    public function gender() {
+        return $this->belongsTo(Gender::class, 'gender_id');
+    }
+}
