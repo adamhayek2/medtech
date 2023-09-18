5e044520bb398066bad5c2439ff552d9f0e34e97
adamhayek2002 <YOUR_EMAIL>
2023-09-18 12:04:58 +0300

    Back-End <> Adding Search Option for Staff

--- server/app/Http/Controllers/StaffController.php
+++ server/app/Http/Controllers/StaffController.php
@@ -7,8 +7,19 @@ use App\Models\Staff;
 
 class StaffController extends Controller
 {
-    public function getStaff() {
-        $staff = Staff::with(['user.userType', 'department', 'gender'])->get();
+    public function getStaff(Request $request) {
+        $query = Staff::with(['user.userType', 'department', 'gender']);
+
+        if ($request->has('search')) {
+            $search = $request->input('search');
+            $query->where(function ($query) use ($search) {
+                $query->where('first_name', 'like', "%$search%")
+                      ->orWhere('last_name', 'like', "%$search%")
+                      ->orWhere('email', 'like', "%$search%");
+            });
+        }
+
+        $staff = $query->get();
 
         return response()->json([
             "status" => "success", 
