807d2d86eac6b549c23f9803e6870359fe3b96fe
adamhayek2002 <YOUR_EMAIL>
2023-09-19 12:01:15 +0300

    Front-End <> Linking the Dashboard API in the Dashboard comonent

--- client/src/component/DashboardComponent/index.jsx
+++ client/src/component/DashboardComponent/index.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import PageTitle from '../PageTite'
 import DashboardStatisticsCard from '../DashboardStatisticsCard'
 import LineChart from '../LineChart'
+import GetDashboard from '../../apis/GetDashboard';
 
 const DashboardComponent = () => {
+  const [dashboardData, setDashboardData] = useState([]);
+  const [error, setError] = useState(false);
+
+  const fetchDashboardData = async () => {
+    try {
+      setError(false); 
+      const response = await GetDashboard(); 
+      setDashboardData(response)
+    } catch (error) {
+      setError(true); 
+    }
+  }
+
+  useEffect(() => {
+    fetchDashboardData();
+  }, []);
 
   return (
     <div className='min-h-screen w-5/6 flex flex-col gap-14 bg-grey p-14 ml-auto'>
