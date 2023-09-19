55bfb7b183151c0c3c84ef8f174106cbb04bb97e
adamhayek2002 <YOUR_EMAIL>
2023-09-19 21:40:51 +0300

    Front-End <> Changing the logic of the Side Navigation Bar

--- client/src/component/SideNav/index.jsx
+++ client/src/component/SideNav/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {useNavigate,useLocation} from 'react-router-dom';
 import { ReactComponent as DashboardSVG } from "../../resources/svg/dashboard_icon.svg";
 import { ReactComponent as EmployeesSVG } from "../../resources/svg/employees_icon.svg";
 import { ReactComponent as ScheduleSVG } from "../../resources/svg/schedule_icon.svg";
@@ -6,36 +7,64 @@ import { ReactComponent as ReportSVG } from "../../resources/svg/reports_icon.sv
 import { ReactComponent as PatientSVG } from "../../resources/svg/patients_icon.svg";
 
 const SideNav = ({active}) => {
+    const navItems = [
+        {
+          text: 'Dashboard',
+          path: '/dashboard',
+          icon: <DashboardSVG />,
+        },
+        {
+          text: 'Employees',
+          path: '/employees',
+          icon: <EmployeesSVG />,
+        },
+        {
+          text: 'Schedule',
+          path: '/schedule',
+          icon: <ScheduleSVG />,
+        },
+        {
+          text: 'Reports',
+          path: '/reports',
+          icon: <ReportSVG />,
+        },
+        {
+          text: 'Patients',
+          path: '/patients',
+          icon: <PatientSVG />,
+        },
+      ];
+
+    const navigate = useNavigate();
+
+    const openReport = (path) => {
+        navigate(path);
+    }
+
+    const location  = useLocation();
+    
     const role = localStorage.getItem('role')
-    console.log(role)
+    
   return (
-    <div className='fixed w-1/6 h-screen bg-secondary flex flex-col p-6 text-white'> 
-        <div className='flex flex-col gap-10 items-start'>
-            {role === "admin" ? (
-                <>
-                <div className={`${active === 'Dashboard' ? 'font-bold' : '' } flex flex-row justify-center items-center gap-3`}>
-                    <DashboardSVG/>
-                    <p className={`${active === 'Dashboard' ? 'text-3xl' : 'text-2xl' }`}>Dashboard</p>
-                </div>
-                <div className={`${active === 'Employees' ? 'font-bold' : '' } flex flex-row justify-center items-center gap-3`}>
-                    <EmployeesSVG/>
-                    <p className={`${active === 'Employees' ? 'text-3xl' : 'text-2xl' }`}>Employees</p>
-                </div>
-                </>
-            ) : "" }
-            <div className={`${active === 'Schedule' ? 'font-bold' : '' } flex flex-row justify-center items-center gap-3`}>
-                <ScheduleSVG/>
-                <p className={`${active === 'Schedule' ? 'text-3xl' : 'text-2xl' }`}>Schedule</p>
+    <div className="fixed w-1/6 h-screen bg-secondary flex flex-col p-6 text-white">
+      <div className="flex flex-col gap-10 items-start">
+        {navItems.map((item) => {
+          return (
+            <div
+              className={`${item.path === location.pathname ? 'font-bold' : ''} flex flex-row justify-center items-center gap-3 cursor-pointer`}
+              onClick={() => {
+                openReport(item.path);
+              }}
+              key={item.path}
+            >
+              {item.icon}
+              <p className={`${item.path === location.pathname ? 'text-3xl' : 'text-2xl'}`}>
+                {item.text}
+              </p>
             </div>
-            <div className={`${active === 'Report' ? 'font-bold' : '' } flex flex-row justify-center items-center gap-3`}>
-                <ReportSVG/>
-                <p className={`${active === 'Report' ? 'text-3xl' : 'text-2xl' }`}>Reports</p>
-            </div>
-            <div className={`${active === 'Patients' ? 'font-bold' : '' } flex flex-row justify-center items-center gap-3`}>
-                <PatientSVG/>
-                <p className={`${active === 'Patients' ? 'text-3xl' : 'text-2xl' }`}>Patients</p>
-            </div>
-        </div>
+          );
+        })}
+      </div>
     </div>
   )
 }
