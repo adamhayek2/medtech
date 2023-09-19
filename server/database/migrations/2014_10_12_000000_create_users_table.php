b3b0e25b8a5d38cf83d25657a89a1facca45fed0
adamhayek2002 <YOUR_EMAIL>
2023-09-20 01:06:45 +0300

    Back-End <> Fixing the user name to unique in migrations

--- server/database/migrations/2014_10_12_000000_create_users_table.php
+++ server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -10,7 +10,7 @@ return new class extends Migration {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->integer('user_type_id');
-            $table->string('username');
+            $table->string('username')->unique();
             $table->string('password');
             $table->timestamps();
 
