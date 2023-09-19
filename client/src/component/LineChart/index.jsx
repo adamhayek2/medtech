5550815caef260c3a22776dd6fb22227f7249572
adamhayek2002 <YOUR_EMAIL>
2023-09-19 11:22:44 +0300

    Front-End <> Removing legend in Line Chart Component

--- client/src/component/LineChart/index.jsx
+++ client/src/component/LineChart/index.jsx
@@ -77,32 +77,6 @@ const LineChart = () => {
             areaBaselineValue={30}
             areaOpacity={0.15}
             useMesh={true}
-            legends={[
-                {
-                    anchor: 'bottom-right',
-                    direction: 'column',
-                    justify: false,
-                    translateX: 100,
-                    translateY: 0,
-                    itemsSpacing: 0,
-                    itemDirection: 'left-to-right',
-                    itemWidth: 80,
-                    itemHeight: 20,
-                    itemOpacity: 0.75,
-                    symbolSize: 12,
-                    symbolShape: 'circle',
-                    symbolBorderColor: '#3540d8',
-                    effects: [
-                        {
-                            on: 'hover',
-                            style: {
-                                itemBackground: 'rgba(0, 0, 0, .03)',
-                                itemOpacity: 1
-                            }
-                        }
-                    ]
-                }
-            ]}
         />
     )
 }
