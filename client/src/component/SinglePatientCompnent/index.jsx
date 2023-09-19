5d541e86e7a222ee9353ac17b214c6d26d459f6e
adamhayek2002 <YOUR_EMAIL>
2023-09-19 18:26:29 +0300

    Front-End <> Creating the HTML Structure of the single of the single patient component

--- client/src/component/SinglePatientCompnent/index.jsx
+++ client/src/component/SinglePatientCompnent/index.jsx
@@ -23,7 +23,21 @@ const SinglePatientComponent = () => {
   }, []);
 
   return (
-    <div>
+    <div className='min-h-screen w-5/6 flex flex-col gap-14 bg-grey p-14 ml-auto'>
+        <div className='w-full flex flex-row justify-between'>
+          <PageTitle title={"Patients"}/>
+        </div>
+        {!reports || error || reports.length === 0? 
+          <div className='w-full h-full flex flex-col items-center'>
+            <NotFoundSVG width={'400px'} height={'400px'} className='opacity-50'/>
+            <div className='text-[36px] font-bold text-primary opacity-1'>No Records</div>
+          </div> : 
+          <div className={`flex flex-row flex-wrap ${reports.length <= 4 ? 'gap-10 justify-start' :'justify-between'}`}>
+          {reports.map((report) => (
+            <ReportCard key={report.id} patientName={report.full_name} id={report.id} status={report.status} report={report.report_data} date={report.time}/>
+          ))}
+          </div>
+        }
     </div>
   )
 }
