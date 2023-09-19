4fa441eaf997e29acde97d4359cf4249bc677d13
adamhayek2002 <YOUR_EMAIL>
2023-09-20 00:57:31 +0300

    Back-End <> Importing the department model to the seeder

--- server/database/seeders/DepartmentsTableSeeder.php
+++ server/database/seeders/DepartmentsTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+Use App\Models\Department;
 
 class DepartmentsTableSeeder extends Seeder
 {
