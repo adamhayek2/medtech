c4ad9b906684bd3c3da97747df497a987bfbc31c
adamhayek2002 <YOUR_EMAIL>
2023-09-20 00:59:43 +0300

    Back-End <> Disabling time stamp in status model

--- server/app/Models/Status.php
+++ server/app/Models/Status.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Status extends Model
 {
     use HasFactory;
+    public $timestamps = false;
 
     public function patients() {
         return $this->hasMany(Patient::class, 'status_id');
