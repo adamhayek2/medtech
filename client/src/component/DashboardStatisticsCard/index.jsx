0caed44891d7f36aaae4d8244d65869ccaf0c313
adamhayek2002 <YOUR_EMAIL>
2023-09-19 09:58:41 +0300

    Front-End <> Adding fixed width to the DashboardStatisticsCard

--- client/src/component/DashboardStatisticsCard/index.jsx
+++ client/src/component/DashboardStatisticsCard/index.jsx
@@ -4,7 +4,7 @@ import { ReactComponent as VirusSVG } from "../../resources/svg/virus.svg";
 
 const DashboardStatisticsCard = ({about, number, label}) => {
   return (
-    <div className='flex flex-col gap-9 py-12 px-5 bg-white rounded-lg'>
+    <div className='w-1/6 flex flex-col gap-9 py-12 px-5 bg-white rounded-lg'>
         {about === "virus" ?  <VirusSVG/> : <PatientDiseaseSVG/>}
         <div className='text-[32px] text-primary font-bold'>{number}</div>
         <div className='text-xl text-black font-bold'>{label}</div>
