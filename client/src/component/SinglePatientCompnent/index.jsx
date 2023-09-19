1bb1975a147ae234232d5e27ba99f94f14690f61
adamhayek2002 <YOUR_EMAIL>
2023-09-19 18:25:49 +0300

    Front-End <> Creating the logic in sigle patient component to handle the response from it's API

--- client/src/component/SinglePatientCompnent/index.jsx
+++ client/src/component/SinglePatientCompnent/index.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { ReactComponent as NotFoundSVG } from "../../resources/svg/not_found.svg";
+import PageTitle from '../PageTite'
+import ReportCard from '../ReportCard'
+import GetSpecificReports from '../../apis/GetSpecificReports';
+
 
 const SinglePatientComponent = () => {
+  const [reports, setReports] = useState([]);
+  const [error, setError] = useState(false);
+
+  const fetchReports = async () => {
+    try {
+      setError(false); 
+      const response = await GetSpecificReports(); 
+      setReports(response)
+    } catch (error) {
+      setError(true); 
+    }
+  }
+  useEffect(() => {
+      fetchReports();
+  }, []);
+
   return (
     <div>
     </div>
