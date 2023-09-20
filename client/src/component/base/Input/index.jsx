533c9bcb065df24875f2d610c91d5690f1fbb069
adamhayek2002 <YOUR_EMAIL>
2023-09-20 21:53:41 +0300

    Front-End <> Adding Inputs to add addStaff form

--- client/src/component/base/Input/index.jsx
+++ client/src/component/base/Input/index.jsx
@@ -4,12 +4,12 @@ const Input = ({ type, placeholder, value, onChange, theme }) => {
   return (
     <div className={`${theme === 'blue' ? 'border-primary' : 'border-white/50' } group relative h-14 w-full rounded-md border  focus-within:border-white focus-within:ring-1 focus-within:ring-primary`}>
       <label
-        className= {`${value === "" ? `absolute left-2 top-1/2 z-0 -translate-y-1/2 bg-transparent  px-1 text-base pointer-events-none duration-200 group-focus-within:top-0 group-focus-within:text-xs ${theme === 'blue' ? 'text-primary group-focus-within:text-primary group-focus-within:bg-white' : 'text-white group-focus-within:text-white group-focus-within:bg-[#3540D8]' }` : "absolute left-2 z-0 -translate-y-1/2  text-white px-1 pointer-events-none duration-200 top-0 text-xs text-white bg-[#3540D8]"}`}
+        className= {`${value === "" ? `absolute left-2 top-1/2 z-0 -translate-y-1/2 bg-transparent  px-1 text-base pointer-events-none duration-200 group-focus-within:top-0 group-focus-within:text-xs ` : "absolute left-2 z-0 -translate-y-1/2  px-1 pointer-events-none duration-200 top-0 text-xs bg-[#3540D8]"} ${theme === 'blue' ? 'text-primary group-focus-within:text-primary group-focus-within:bg-white bg-white text-primary' : 'text-white group-focus-within:text-white group-focus-within:bg-[#3540D8]' }`}
       >
         {placeholder}
       </label> 
       <input 
-        className='z-10 h-full w-full rounded-md px-3.5 py-4 outline-none bg-transparent text-white' 
+        className={`${theme === 'blue' ? 'text-primary' : 'text-white' }z-10 h-full w-full rounded-md px-3.5 py-4 outline-none bg-transparent `} 
         type={type} 
         value={value}
         onChange={onChange}
--- client/src/component/modals/AddStaff/index.jsx
+++ client/src/component/modals/AddStaff/index.jsx
@@ -8,10 +8,10 @@ const AddStaff = ({open, onClose}) => {
     const [email, setEmail] = useState('');
     const [phoneNumber, setphoneNumber] = useState('');
     const [gender, setGender] = useState();
-    const [dateOfBirth, setDateOFBirth] = useState();
+    const [dateOfBirth, setDateOfBirth] = useState();
     const [department, setDepartment] = useState();
     const [major, setMajor] = useState('');
-    const [user_type, setUserTYpe] = useState();
+    const [user_type, setUserType] = useState();
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState();
@@ -22,10 +22,10 @@ const AddStaff = ({open, onClose}) => {
         setEmail('');
         setphoneNumber('');
         setGender('');
-        setDateOFBirth('');
+        setDateOfBirth('');
         setDepartment('');
         setMajor('');
-        setUserTYpe('');
+        setUserType('');
         setUsername('');
         setPassword('');
     };
@@ -35,11 +35,51 @@ const AddStaff = ({open, onClose}) => {
     return (
         <div onClick={onClose} className='flex flex-row justify-end fixed w-full h-full bg-[#000000]/30 z-10 top-0 left-0 '>
             <form 
-                onClick={(e) => {
-                    e.stopPropagation()
-                }}
-                className={`flex flex-col bg-white w-1/4 p-10 justify-center items-center transition-transform duration-[0.2s] ease-[ease-in-out] ${!open ? 'translate-x-full' : 'translate-x-0'}`}
+                onClick={(e) => { e.stopPropagation() }}
+                className={`flex flex-col bg-white w-1/4 p-10 justify-center items-center transition-transform duration-[0.2s] ease-[ease-in-out] gap-6 ${!open ? 'translate-x-full' : 'translate-x-0'}`}
             >
+                <div className=' flex flex-row gap-3'>
+                    <Input
+                    name="First name"
+                    value={fistName}
+                    type={"text"}
+                    onChange={(e) => setFirstName(e.target.value)}
+                    placeholder="First name"
+                    theme={"blue"}
+                    />
+                    <Input
+                    name="Last name"
+                    value={lastName}
+                    type={"text"}
+                    onChange={(e) => setLastName(e.target.value)}
+                    placeholder="Last name"
+                    theme={"blue"}
+                    />
+                </div>
+                <Input
+                name="email"
+                value={email}
+                type={"email"}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="E-mail"
+                theme={"blue"}
+                />
+                <Input
+                name="phone_number"
+                value={phoneNumber}
+                type={"email"}
+                onChange={(e) => setphoneNumber(e.target.value)}
+                placeholder="Phone number"
+                theme={"blue"}
+                />
+                <Input
+                name="date_of_birth"
+                value={dateOfBirth}
+                type={"date"}
+                onChange={(e) => setDateOfBirth(e.target.value)}
+                placeholder="Date of birth"
+                theme={"blue"}
+                />
                 <Input
                 name="username"
                 value={username}
@@ -48,6 +88,23 @@ const AddStaff = ({open, onClose}) => {
                 placeholder="Username"
                 theme={"blue"}
                 />
+                <Input
+                name="password"
+                value={password}
+                type={"password"}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+                theme={"blue"}
+                />
+                <Input
+                name="major"
+                value={major}
+                type={"text"}
+                onChange={(e) => setMajor(e.target.value)}
+                placeholder="Major"
+                theme={"blue"}
+                />
+                
                 {/* <Button label={'Edit Password'} BgColor={'bg-primary'} textColor={'text-white'}/> */}
             </form>
         </div>
