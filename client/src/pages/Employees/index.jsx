8e4454f5280cd2fb858fa1148e50600b6dd18dd4
adamhayek2002 <YOUR_EMAIL>
2023-09-20 13:05:12 +0300

    Front-End <> Adding NavBar and SideNav to the Employees page

--- client/src/pages/Employees/index.jsx
+++ client/src/pages/Employees/index.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import NavBar from '../../component/NavBar'
+import SideNav from '../../component/SideNav'
 
 const Employees = () => {
   return (
-    <div>Employees</div>
+    <div className='flex flex-col font-futur'>
+      <NavBar/> 
+      <div className='flex flex-row'>
+        <SideNav/>
+      </div>
+    </div>
   )
 }
 
