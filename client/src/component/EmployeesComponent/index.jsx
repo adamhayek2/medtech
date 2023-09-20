e629f083fcdad5fbc13df6e8c117b7d721abe18e
adamhayek2002 <YOUR_EMAIL>
2023-09-20 15:49:15 +0300

    Front-End <> Implementing Employee card and Handling the displaying

--- client/src/component/EmployeesComponent/index.jsx
+++ client/src/component/EmployeesComponent/index.jsx
@@ -3,6 +3,7 @@ import { useDebounce } from "@uidotdev/usehooks";
 import GetStaff from '../../apis/GetStaff';
 import PageTitle from '../PageTite';
 import SearchInput from '../base/SearchInput';
+import PatienCard from '../PatientCard';
 import { ReactComponent as NotFoundSVG } from "../../resources/svg/not_found.svg";
 import EmployeeCard from '../EmployeeCard';
 
@@ -48,6 +49,23 @@ const EmployeesComponent = () => {
                 <PageTitle title={"Employees"}/>
                 <SearchInput onChange={(e) => {setSearchValue(`?search=${e.target.value}`)}}/>
             </div>
+            {!employees || employees.length === 0 || error ? 
+                <div className='w-full h-full flex flex-col items-center'>
+                <NotFoundSVG width={'400px'} height={'400px'} className='opacity-50'/>
+                <div className='text-[36px] font-bold text-primary opacity-1'>No Records</div>
+                </div> : 
+                <div className={`flex flex-row flex-wrap ${employees.length <= 4 ? 'gap-10 justify-start' :'justify-between'}`}>
+                {employees.map((employee) => (
+                    <EmployeeCard 
+                        key={employee.id} 
+                        id={employee.id} 
+                        name={employee.user.user_type === 2 ? `Dr. ${employee.first_name} ${employee.last_name}` : `${employee.first_name} ${employee.last_name}`} 
+                        major={employee.major} 
+                        department={employee.department.name} 
+                        hireDate={employee.hired_at}/>
+                    ))}
+                </div> 
+            }   
         </div>
       )
 }
