52bbd728d84c96d0304ebec4c9ccbaa311d2230a
adamhayek2002 <YOUR_EMAIL>
2023-09-20 16:42:48 +0300

    Front-End <> Creating the employees profile API

---
+++ client/src/apis/EmployeeProfile/index.jsx
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const EmployeeProfile = async ({id}) => {
+    try {
+        const response = await axios.get(`http://127.0.0.1:8000/api/admin/staff_profile${id}`, {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`
+            }
+          });
+      return response.data.data;
+    } catch (error) {
+        throw error
+    }
+}
+
+export default EmployeeProfile
