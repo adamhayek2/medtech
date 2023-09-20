9edd27f1e6b6af1060bca453f3aa15e3f1c0f368
adamhayek2002 <YOUR_EMAIL>
2023-09-20 16:55:27 +0300

    Front-End <> Linking the StaffProfile API to the EmployeeProfile

--- client/src/component/EmployeeProfileComponent/index.jsx
+++ client/src/component/EmployeeProfileComponent/index.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { useParams } from 'react-router-dom';
+import StaffProfile from '../../apis/StaffProfile';
 
 const EmployeeProfileComponent = () => {
+    const { id } = useParams();
+    const [employee, setEmployee] = useState([]);
+    const [error, setError] = useState(false);
+
+    const fetchEmployeeProfile = async () => {
+        try {
+          setError(false); 
+          const response = await StaffProfile({id}); 
+          setEmployee(response)
+          console.log(response)
+        } catch (error) {
+          setError(true); 
+        }
+    }
+
+    useEffect(() => {
+        fetchEmployeeProfile();
+      }, []);
+
   return (
     <div>EmployeeProfileComponent</div>
   )
