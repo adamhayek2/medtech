50322996e58ca278d71ee4dc4d1c218acbf75a8d
adamhayek2002 <YOUR_EMAIL>
2023-09-20 16:53:57 +0300

    Front-End <> Fixing missed / in StaffProfile API

--- client/src/apis/StaffProfile/index.jsx
+++ client/src/apis/StaffProfile/index.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const StaffProfile = async ({id}) => {
     try {
-        const response = await axios.get(`http://127.0.0.1:8000/api/admin/staff_profile${id}`, {
+        const response = await axios.get(`http://127.0.0.1:8000/api/admin/staff_profile/${id}`, {
             headers: {
               Authorization: `Bearer ${localStorage.getItem("token")}`
             }
