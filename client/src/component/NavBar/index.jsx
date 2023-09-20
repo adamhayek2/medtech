95a622596901fa874ea0ffcc1b9ceb0ea39aafc6
adamhayek2002 <YOUR_EMAIL>
2023-09-20 06:26:27 +0300

    Front-End <> Adding a drop down menu for the user

--- client/src/component/NavBar/index.jsx
+++ client/src/component/NavBar/index.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { ReactComponent as LogoSVG } from "../../resources/svg/logo.svg";
 import { ReactComponent as NotificationSVG } from "../../resources/svg/notification-thin.svg";
 import { ReactComponent as Profile } from "../../resources/svg/profile.svg";
 
 const NavBar = () => {
+  const [showOptions, setShowOptions] = useState(false);
+
+  const handleMouseEnter = () => {
+    setShowOptions(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowOptions(false);
+  };
+
+  const handleSeeProfileClick = () => {
+    // Implement the action for "See Profile" here
+  };
+
+  const handleLogoutClick = () => {
+    // Implement the action for "Logout" here
+  };
+
   return (
     <div className='sticky top-0 flex-no-wrap relative flex w-full items-center justify-between bg-primary py-2 shadow-md shadow-black/5 dark:bg-neutral-600 dark:shadow-black/10 lg:flex-wrap lg:justify-start lg:py-6 z-10'>
         <div className='flex w-full flex-wrap items-center justify-between px-6'>
@@ -13,10 +31,22 @@ const NavBar = () => {
             </div>
             <div className='flex justify-center items-center gap-6'>
                 <NotificationSVG height={"35px"} width={"32px"}/>
-                <Profile height={"35px"}/>
+                <div onMouseEnter={handleMouseEnter}
+                    >
+                  <Profile height={"35px"}/>
+                  {showOptions && (
+                    <div className="flex flex-col gap-3 absolute translate-x-[-120px] translate-y-[5px] bg-white w-40 rounded-lg"
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}>
+                      <button onClick={handleSeeProfileClick} className='px-6 py-3 hover:bg-primary/20 hover:text-primary hover:font-bold transition transition ease-in-out transition-delay-none duration-150'>See Profile</button>
+                      <button onClick={handleLogoutClick} className='px-6 py-3 hover:bg-primary/20 hover:text-primary hover:font-bold transition transition ease-in-out transition-delay-none duration-150'>Logout</button>
+                    </div>
+                  )}
+                </div>
+              </div>
             </div>
         </div>
-    </div>
+    
   )
 }
 
