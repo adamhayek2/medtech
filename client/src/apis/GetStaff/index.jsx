40438b26c74f115ccb93cce2ea0b5d403c0b4567
adamhayek2002 <YOUR_EMAIL>
2023-09-20 13:11:14 +0300

    Front-End <> Creating The GetStaff API

---
+++ client/src/apis/GetStaff/index.jsx
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+
+const SingleReport = async ({filter}) => {
+    try {
+        const response = await axios.get(`http://127.0.0.1:8000/api/admin/get_staff${filter}`, {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`
+            }
+          });
+      return response.data.data;
+    } catch (error) {
+        throw error
+    }
+}
+
+export default SingleReport
