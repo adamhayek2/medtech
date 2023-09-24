94267ee298adfdf7dc63911b87091e888362f4d8
adamhayek2002 <YOUR_EMAIL>
2023-09-25 01:04:32 +0300

    Front-End <> Modifying backgroud color for notifications and modal

--- client/src/component/base/SingleNotification/index.jsx
+++ client/src/component/base/SingleNotification/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SingleNotification = () => {
   return (
-    <div className={`flex w-full h-[100px] flex-col justify-center items-start pl-6 pr-[217px] pt-0 pb-6 rounded-lg border-l-[12px] border-l-primary border-solid`}>
+    <div className={`flex w-full h-[100px] flex-col justify-center items-start pl-6 pr-[217px] pt-0 pb-6 rounded-lg border-l-[12px] border-l-primary border-solid bg-white`}>
         <div className={`text-primary text-[28px] font-normal leading-[normal]`}>title</div>
         <div className={`text-[#000000]/70 text-base font-normal leading-[normal]`}>body</div>
     </div>
