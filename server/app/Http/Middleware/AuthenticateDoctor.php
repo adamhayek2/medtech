791cee293eb3a70efd7ad74a436408134f93c39f
adamhayek2002 <YOUR_EMAIL>
2023-09-24 11:40:36 +0300

    Back-End <> Fixing issues with doctor authentication

--- server/app/Http/Middleware/AuthenticateDoctor.php
+++ server/app/Http/Middleware/AuthenticateDoctor.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Auth;
 
 class AuthenticateDoctor
 {
@@ -16,7 +17,7 @@ class AuthenticateDoctor
     public function handle(Request $request, Closure $next): Response {
         $user = Auth::user();
 
-        if($user->user_type_id == 1){
+        if($user->user_type_id == 2){
             return $next($request);
         }
         
