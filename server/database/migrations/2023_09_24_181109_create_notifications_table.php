fb5dda36063982b9883b921b001d18697a6679c8
adamhayek2002 <YOUR_EMAIL>
2023-09-24 21:48:51 +0300

    Back-End <> Defining the table schema for noifications table

--- server/database/migrations/2023_09_24_181109_create_notifications_table.php
+++ server/database/migrations/2023_09_24_181109_create_notifications_table.php
@@ -4,24 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
+return new class extends Migration {
+    public function up(): void {
         Schema::create('notifications', function (Blueprint $table) {
             $table->id();
+            $table->integer('staff_id');
+            $table->boolean('seen')->default(false);
+            $table->string('title');
+            $table->string('body');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('notifications');
     }
 };
