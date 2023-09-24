325c5a3b9c28a10537852440068c9709449e3ae3
adamhayek2002 <YOUR_EMAIL>
2023-09-25 00:54:14 +0300

    Front-End <> Adding HTML structure and Styles to SingleNotification

--- client/src/component/base/SingleNotification/index.jsx
+++ client/src/component/base/SingleNotification/index.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const SingleNotification = () => {
   return (
-    <div>SingleNotification</div>
+    <div className={`flex w-full h-[100px] flex-col justify-center items-start pl-6 pr-[217px] pt-0 pb-6 rounded-lg border-l-[12px] border-l-primary border-solid`}>
+        <div className={`text-primary text-[28px] font-normal leading-[normal]`}>title</div>
+        <div className={`text-[#000000]/70 text-base font-normal leading-[normal]`}>body</div>
+    </div>
   )
 }
 
--- client/src/component/modals/Notifications/index.jsx
+++ client/src/component/modals/Notifications/index.jsx
@@ -4,7 +4,9 @@ const Notifications = ({open, onClose}) => {
   if(!open) return null
   return (
     <div onClick={onClose} className='flex flex-row justify-end fixed w-full min-h-full bg-[#000000]/30 z-10 top-0 left-0 overflow-y-scroll'>
-
+      <div className={`flex flex-col bg-white w-1/4 p-10 justify-center items-center transition-transform duration-[0.2s] ease-[ease-in-out] gap-6 ${!open ? 'translate-x-full' : 'translate-x-0'}`}>
+        
+      </div>
     </div>
   )
 }
