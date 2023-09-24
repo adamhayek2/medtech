7efe4adbe86e513d1b466b59e0b8ad97f29639a0
adamhayek2002 <YOUR_EMAIL>
2023-09-24 14:17:35 +0300

    Front-End <> Adding a toaster to display notifications

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Authentication from './pages/Authentication';
 import Home from './pages/Home';
@@ -9,36 +9,45 @@ import Dashboard from './pages/Dashboard';
 import SinglePatient from './pages/SinglePatient';
 import Employees from './pages/Employees';
 import EmployeeProfile from './pages/EmployeeProfile';
-import {fetchToken, onMessageListener} from './utils/initializingFirebase';
+import { fetchToken, onMessageListener } from './utils/initializingFirebase';
+import toast, { Toaster } from 'react-hot-toast';
 
 function App() {
-  const fcm_token = localStorage.getItem('fcm_token')
+  const [notification, setNotification] = useState([]);
+  const fcm_token = localStorage.getItem('fcm_token');
 
   useEffect(() => {
-    if(!fcm_token) fetchToken(); 
-    const not = onMessageListener().then(payload => {
-      console.log('message recieved', payload)
-      return () => {
-        not.catch(err => console.log('failed: ', err));
-      }
-    })
-  }, []);
+    if (!fcm_token) fetchToken();
+    const not = onMessageListener().then((payload) => {
+      const receivedNotification = {
+        title: payload?.notification?.title,
+        body: payload?.notification?.body,
+      };
+      console.log(receivedNotification);
+    
+    });
+  }, [notification]);
+
   return (
-    <BrowserRouter> 
-      <Routes>
-        <Route path="/" element={<Authentication />}/> 
-        <Route path="/patients" element={<Home/>}/> 
-        <Route path="/patient/:id" element={<SinglePatient/>}/> 
-        <Route path="/reports" element={<Reports/>}/> 
-        <Route path="/report/:id" element={<SingleReport/>}/> 
-        <Route path="/schedule" element={<Schedule/>}/> 
+    <>
+     <Toaster position='top-center'/>
+      <BrowserRouter> 
+        <Routes>
+          <Route path="/" element={<Authentication />}/> 
+          <Route path="/patients" element={<Home/>}/> 
+          <Route path="/patient/:id" element={<SinglePatient/>}/> 
+          <Route path="/reports" element={<Reports/>}/> 
+          <Route path="/report/:id" element={<SingleReport/>}/> 
+          <Route path="/schedule" element={<Schedule/>}/> 
 
 
-        <Route path="/dashboard" element={<Dashboard/>}/> 
-        <Route path="/employees" element={<Employees/>}/> 
-        <Route path="/employee/:id" element={<EmployeeProfile/>}/> 
-      </Routes>
-    </BrowserRouter> 
+          <Route path="/dashboard" element={<Dashboard/>}/> 
+          <Route path="/employees" element={<Employees/>}/> 
+          <Route path="/employee/:id" element={<EmployeeProfile/>}/> 
+        </Routes>
+      </BrowserRouter> 
+    </>
+    
   );
 }
 
