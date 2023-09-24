2b334cdd1de01627ec57b9fba7edd59dafcfe30c
adamhayek2002 <YOUR_EMAIL>
2023-09-25 00:35:43 +0300

    Front-End <> Creating an API to fetch notifications from back-end

---
+++ client/src/apis/GetNotifications/index.jsx
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const apiUrl = 'http://127.0.0.1:8000/api/user/all_notifications';
+
+const GetNotifications = async () => {
+    try {
+        const response = await axios.get(apiUrl, {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`
+            }
+          });
+
+      return response.data.data;
+    } catch (error) {
+      throw error    
+    }
+  };
+export default GetNotifications
