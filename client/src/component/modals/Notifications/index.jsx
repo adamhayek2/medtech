803fe4e2f03dbc696bc52b913b7ec34cd581b278
adamhayek2002 <YOUR_EMAIL>
2023-09-25 11:11:32 +0300

    Front-End <> Finxing the height for not found SVG

--- client/src/component/modals/Notifications/index.jsx
+++ client/src/component/modals/Notifications/index.jsx
@@ -29,7 +29,7 @@ const Notifications = ({open, onClose}) => {
     <div onClick={onClose} className='flex flex-row justify-end fixed w-full min-h-full bg-[#000000]/30 z-10 top-0 left-0 overflow-y-scroll'>
         {!notifications || notifications.length === 0 || error ? 
           <div className={`flex flex-col bg-grey w-1/4 p-10 justify-center items-center transition-transform duration-[0.2s] ease-[ease-in-out] gap-6 translate-x-0 ${!open ? 'translate-x-full' : ''}`}>            
-            <NotFoundSVG width={'200px'} height={'42000px'} className='opacity-50'/>
+            <NotFoundSVG width={'200px'} height={'200px'} className='opacity-50'/>
             <div className='text-[24px] font-bold text-primary opacity-50'>No notifications yet!</div>
           </div> : 
           <div className={`flex flex-col bg-grey w-1/4 p-10 justify-center items-center transition-transform duration-[0.2s] ease-[ease-in-out] gap-6 ${!open ? 'translate-x-full' : 'translate-x-0'}`}>            
