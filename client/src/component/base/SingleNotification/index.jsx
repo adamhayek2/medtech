f45687769b95d8295950f19fa69438d7cad0177b
adamhayek2002 <YOUR_EMAIL>
2023-09-25 18:09:37 +0300

    Front-End <> Adding styles for notification that are seen

--- client/src/component/base/SingleNotification/index.jsx
+++ client/src/component/base/SingleNotification/index.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const SingleNotification = ({notification}) => {
   return (
-    <div className={`flex w-full h-[100px] flex-col justify-center items-start pl-6 pr-[217px] pt-0 pb-6 rounded-lg border-l-[12px] border-l-primary border-solid bg-white`}>
-        <div className={`text-primary text-[28px] font-normal leading-[normal]`}>{notification.title}</div>
-        <div className={`text-[#000000]/70 text-base font-normal leading-[normal]`}>{notification.body}</div>
+    <div className={`flex w-full h-[100px] flex-col justify-center items-start pl-6 pr-[217px] pt-0 pb-2 rounded-lg ${notification.seen ? '': 'border-l-[12px] border-l-primary border-solid '} border-[0.5px] border-solid border-[#000000]/30 bg-white `}>
+        <div className={`text-[28px] font-normal leading-[normal] ${notification.seen ? 'text-primary/80 ': 'text-primary'}`}>{notification.title}</div>
+        <div className={`text-base font-normal leading-[normal] ${notification.seen ? 'text-[#000000]/50 ': 'text-[#000000]/70'}`}>{notification.body}</div>
     </div>
   )
 }
