5e9e59fa64e7f5313690d8d396332618e6c8d631
adamhayek2002 <YOUR_EMAIL>
2023-09-25 20:28:17 +0300

    Arduino <> Defining the motors speed

--- arduino/medtech/medtech.ino
+++ arduino/medtech/medtech.ino
@@ -2,6 +2,9 @@
 #define in 8
 #define in 7
 #define in 6
+#define int m1_speed = 80; // speed of motor 1
+#define int m3_speed = 80; // speed of motor 2
+
 
 void setup() {
   // put your setup code here, to run once:
