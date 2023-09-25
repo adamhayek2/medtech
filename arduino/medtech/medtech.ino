3e1deabb95d9142819bc2bae16f33fec81d767e7
adamhayek2002 <YOUR_EMAIL>
2023-09-25 20:29:47 +0300

    Arduino <> Defining the motors rotation speed

--- arduino/medtech/medtech.ino
+++ arduino/medtech/medtech.ino
@@ -4,6 +4,8 @@
 #define in 6
 #define int m1_speed = 80; // speed of motor 1
 #define int m3_speed = 80; // speed of motor 2
+#define int left_rotation_speed = 250; // speed of left rotation
+#define int right_rotation_speed = 250; // speed of roght rotation
 
 
 void setup() {
