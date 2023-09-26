78e1996fa7992793302d6d7d85ce34096d43a8fa
adamhayek2002 <YOUR_EMAIL>
2023-09-27 00:30:58 +0300

    Arduino <> Inializing pins for the motors

--- arduino/medtech/medtech.ino
+++ arduino/medtech/medtech.ino
@@ -1,3 +1,10 @@
+#define enA 10   // PWM pin for Motor 1
+#define enB 11   // PWM pin for Motor 2
+#define in1 9    // Motor 1 control input 1
+#define in2 8    // Motor 1 control input 2
+#define in3 7    // Motor 2 control input 1
+#define in4 6    // Motor 2 control input 2
+
 void setup() {
   // put your setup code here, to run once:
 
