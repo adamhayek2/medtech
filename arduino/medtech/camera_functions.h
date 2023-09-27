03e97b2b9d09653d5224c498087e64b5c2d0e0dc
adamhayek2002 <YOUR_EMAIL>
2023-09-27 13:11:39 +0300

    Arduino <> Creating header file for camera functions

---
+++ arduino/medtech/camera_functions.h
@@ -0,0 +1,11 @@
+// camera_functions.h
+#ifndef CAMERA_FUNCTIONS_H
+#define CAMERA_FUNCTIONS_H
+
+#include <Arduino.h> // Include necessary libraries
+
+void initializeCamera();
+void captureImage();
+// Define any camera-related variables here
+
+#endif
