12d8ca0a7a484aa41cb1075227d100804a8afef2
adamhayek2002 <YOUR_EMAIL>
2023-09-27 04:59:29 +0300

    Arduino <> Connecting my module to wifi with credentials defined before

--- arduino/medtech/wifi_module/wifi_module.ino
+++ arduino/medtech/wifi_module/wifi_module.ino
@@ -7,3 +7,15 @@ const char* ssid = "";
 const char* password = "";
 
 AsyncWebServer server(80);
+
+void setup() {
+  Serial.begin(115200);
+  
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+}
