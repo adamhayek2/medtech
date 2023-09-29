1b8b7c9f0b96ab447bf1fe3130dcfe634ff4fb52
adamhayek2002 <YOUR_EMAIL>
2023-09-29 05:11:29 +0300

    Front-End <> Adding loading effect to calendar page

--- client/src/component/CalenderComponent/index.jsx
+++ client/src/component/CalenderComponent/index.jsx
@@ -1,10 +1,14 @@
 import React, {useState, useEffect} from 'react'
 import './index.css'
 import { Calendar, momentLocalizer } from 'react-big-calendar'
+import Lottie from 'react-lottie';
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 import moment from 'moment';
 import EventComponent from '../base/EventComponent';
 import UserSchedule from '../../apis/UserSchedule'
+import * as loadingSVG from '../../resources/animations/loading.json'
+import { ReactComponent as NotFoundSVG } from "../../resources/svg/not_found.svg";
+
 
 
 const localizer = momentLocalizer(moment);
@@ -12,9 +16,11 @@ const localizer = momentLocalizer(moment);
 const CalenderComponent = () => {
     const [events, setEvents] = useState([]);
     const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(false);
 
     const eventsHandler = async () => {
       try {
+        setLoading(true);
         const response = await UserSchedule();
         const tem = response.map((slot) => (
           {
@@ -26,9 +32,8 @@ const CalenderComponent = () => {
         console.log(tem)
         setError(false); 
         setEvents(tem)
-        console.log(response)
+        setLoading(false);
       } catch (error) {
-        console.error('error:', error);
         setError(true); 
       }
   }
@@ -39,11 +44,29 @@ const CalenderComponent = () => {
 
   useEffect(() => {
     console.log(events);
-}, [events]);
+  }, [events]);
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true, 
+    animationData: loadingSVG,
+    rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice'
+    }
+  }
 
   return (
     <div className='min-h-screen w-5/6 flex flex-col gap-14 bg-grey p-14 ml-auto'>
-      {events.length === 0 ? "No events" : <Calendar
+      {events.length === 0 ?
+        loading ? 
+        <div className="w-full h-full flex flex-col items-center justify-center">
+          <Lottie options={defaultOptions} height={430} width={515} />
+        </div> : 
+        <div className='w-full h-full flex flex-col items-center'>
+            <NotFoundSVG width={'400px'} height={'400px'} className='opacity-50'/>
+            <div className='text-[36px] font-bold text-primary opacity-1'>No Events</div>
+        </div> : 
+        <Calendar
           localizer={localizer}
           events={events}
           startAccessor="start"
