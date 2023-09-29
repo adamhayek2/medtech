88d6d5c9f44f63f4d8acf1f2c4fbb65af33aa4a3
adamhayek2002 <YOUR_EMAIL>
2023-09-29 04:54:22 +0300

    Front-End <> Adding loading effect to single patient page

--- client/src/component/SinglePatientCompnent/index.jsx
+++ client/src/component/SinglePatientCompnent/index.jsx
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import { useParams } from 'react-router-dom';
+import Lottie from 'react-lottie';
 import { ReactComponent as NotFoundSVG } from "../../resources/svg/not_found.svg";
+import * as loadingSVG from '../../resources/animations/loading.json'
 import PageTitle from '../PageTite'
 import ReportCard from '../ReportCard'
 import GetSpecificReports from '../../apis/GetSpecificReports';
@@ -10,12 +12,15 @@ const SinglePatientComponent = () => {
   const { id } = useParams();
   const [reports, setReports] = useState([]);
   const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const fetchReports = async () => {
     try {
+      setLoading(true);
       setError(false); 
       const response = await GetSpecificReports({id}); 
-      setReports(response)
+      setReports(response);
+      setLoading(false);
     } catch (error) {
       setError(true); 
     }
@@ -25,14 +30,27 @@ const SinglePatientComponent = () => {
       fetchReports();
   }, []);
 
+  const defaultOptions = {
+    loop: true,
+    autoplay: true, 
+    animationData: loadingSVG,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
+
   return (
     
     <div className='min-h-screen w-5/6 flex flex-col gap-14 bg-grey p-14 ml-auto'>
       {!reports || error || reports.length === 0? 
-          <div className='w-full h-full flex flex-col items-center'>
-            <NotFoundSVG width={'400px'} height={'400px'} className='opacity-50'/>
-            <div className='text-[36px] font-bold text-primary opacity-1'>No Records</div>
-          </div> : 
+        loading ? <div className="w-full h-full flex flex-col items-center justify-center">
+        <Lottie options={defaultOptions} height={430} width={515} />
+        </div> : 
+            <div className='w-full h-full flex flex-col items-center'>
+                <NotFoundSVG width={'400px'} height={'400px'} className='opacity-50'/>
+                <div className='text-[36px] font-bold text-primary opacity-1'>No Recored found</div>
+          </div>
+        :
           <>
             <div className='w-full flex flex-row justify-between'>
               <PageTitle title={reports[0].full_name}/>
