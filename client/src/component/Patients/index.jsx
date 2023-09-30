10dddc1571eb9c5cb51c4ead1fc1d13cc239920c
adamhayek2002 <YOUR_EMAIL>
2023-09-30 23:26:28 +0300

    Front-End <> Adding loading animation in patients page

--- client/src/component/Patients/index.jsx
+++ client/src/component/Patients/index.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import { useDebounce } from "@uidotdev/usehooks";
+import Lottie from 'react-lottie';
 import PatienCard from '../PatientCard'
 import PatientSearch from '../../apis/PatientSearch';
 import PageTitle from '../PageTite'
@@ -8,6 +9,7 @@ import GetPatients from '../../apis/GetPatients'
 import SearchInput from '../base/SearchInput'
 import { ReactComponent as NotFoundSVG } from "../../resources/svg/not_found.svg";
 import AddPatientModal from '../modals/addPatientModal';
+import * as loadingSVG from '../../resources/animations/loading.json'
 
 
 const Patients = () => {
@@ -16,12 +18,15 @@ const Patients = () => {
     const [openModal, setOpenModal] = useState(false)
     const [searchValue, setSearchValue] = useState("");
     const debouncedSearchValue = useDebounce(searchValue, 300);
+    const [loading, setLoading] = useState(false);
 
     const fetchPatients = async () => {
     try {
+      setLoading(true);
       setError(false); 
       const response = await GetPatients();
-      setPatient(response)
+      setPatient(response);
+      setLoading(false);
     } catch (error) {
       setError(true); 
       console.error('error:', error);
@@ -47,6 +52,15 @@ const Patients = () => {
       fetchPatients();
     }
   }, [debouncedSearchValue]);
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true, 
+    animationData: loadingSVG,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
   
   return (
     <div className='min-h-screen w-5/6 flex flex-col gap-14 bg-grey p-14 ml-auto'>
@@ -61,6 +75,10 @@ const Patients = () => {
         }
       </div>
       {!patients || patients.length === 0 || error ? 
+      loading ? 
+      <div className="w-full h-full flex flex-col items-center justify-center">
+          <Lottie options={defaultOptions} height={430} width={515} />
+      </div> :
         <div className='w-full h-full flex flex-col items-center'>
           <NotFoundSVG width={'400px'} height={'400px'} className='opacity-50'/>
           <div className='text-[36px] font-bold text-primary opacity-1'>No Records</div>
