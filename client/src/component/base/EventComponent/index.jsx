d639be057e16855306546dde8f1007911dde7b47
adamhayek2002 <YOUR_EMAIL>
2023-09-30 23:39:51 +0300

    Front-End <> Adjusting the style of calendar event

--- client/src/component/base/EventComponent/index.jsx
+++ client/src/component/base/EventComponent/index.jsx
@@ -4,10 +4,10 @@ import moment from 'moment';
 const EventComponent = ({event}) => {
   return (
     <div className='flex flex-row h-full justify-between'>
-      <div className='flex flex-col justify-between'>
-        <div className='text-xl'>{event.title}</div>
-        <div className='text-base'>{event.patient}</div>
-        <div className='text-grey'>
+      <div className='flex flex-col gap-1'>
+        <div className='text-xl font-bold'>{event.title}</div>
+        <div className='text-s'>{event.patient}</div>
+        <div className='text-grey pt-5'>
           {moment(event.start).format('LT')} - {moment(event.end).format('LT')}
         </div>
       </div>
