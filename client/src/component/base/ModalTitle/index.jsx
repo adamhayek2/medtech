9235371b215cfcfdfbeb53dcea310fccb7c5bb18
adamhayek2002 <YOUR_EMAIL>
2023-10-01 09:39:53 +0300

    Front-End <> Creating modal title component

---
+++ client/src/component/base/ModalTitle/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ModalTitle = ({title}) => {
+  return (
+    <div className='w-full text-3xl text-primary font-bold mb-6'>{title}</div>
+  )
+}
+
+export default ModalTitle
